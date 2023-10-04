4f51f554c33e176f55534c7ebd2a3718d9279e78
Harry Allen <66224939+HarryAllen1@users.noreply.github.com>
2023-10-04 11:58:07 -0700

    temp

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
-	import { auth } from '$lib';
+	import { auth, db, events, memberData } from '$lib';
 	import { signOut } from 'firebase/auth';
 	import Button from '../lib/components/button/button.svelte';
+	import { doc, setDoc } from 'firebase/firestore';
+
+	const singedUpEvents = memberData
+		.find((m) => m.email === auth.currentUser?.email)
+		?.events.map((e) => ({
+			...events.find((ev) => ev.event === e)
+		}));
+
+	for (const event of events) {
+		setDoc(doc(db, 'events', event.event), []);
+	}
 </script>
 
-<Button on:click={() => signOut(auth)}>Sign out</Button>
+<div class="mt-8 flex flex-col items-center">
+	<Button on:click={() => signOut(auth)}>Sign out</Button>
+</div>
