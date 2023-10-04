20b4205e18593eb570ce5daf60f13e2328ebc0b9
Harry Allen <66224939+HarryAllen1@users.noreply.github.com>
2023-10-04 09:16:48 -0700

    uhh stuff

---
+++ src/lib/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyA-_aVUnDt3gOHjtoFwO4S1vSGSnZtCvAU',
+	authDomain: 'tsa-grouping-thing.firebaseapp.com',
+	projectId: 'tsa-grouping-thing',
+	storageBucket: 'tsa-grouping-thing.appspot.com',
+	messagingSenderId: '1034710953450',
+	appId: '1:1034710953450:web:20baf0bbc221b34fcfd693',
+	measurementId: 'G-V9TSZ35FNE'
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
