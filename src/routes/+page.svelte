a3315138f2b155bdfc4e27600fce848c97735d5a
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2023-10-04 20:28:38 -0700

    finish?

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,11 @@
 	import { goto } from '$app/navigation';
 	import { auth, db, events, memberData } from '$lib';
 	import { Button } from '$lib/components/button';
+	import { Plus } from 'lucide-svelte';
 	import * as Card from '$lib/components/card';
 	import { signOut } from 'firebase/auth';
+	import * as Tooltip from '$lib/components/tooltip';
+	import * as Dialog from '$lib/components/dialog';
 	import { Doc, userStore } from 'sveltefire';
 	import Alert from '../lib/components/alert/alert.svelte';
 	import AlertTitle from '../lib/components/alert/alert-title.svelte';
@@ -55,71 +58,136 @@
 							<Card.Description>Max {event.maxTeamSize} people per team</Card.Description>
 						</Card.Header>
 						<Card.Content class="flex flex-col gap-4">
-							{#each data.teams as te}
-								{@const team = correctType(te)}
-								<Card.Root class="bg-blue-500 bg-opacity-20">
-									<Card.Title class="m-2 ml-4">
-										{#if team.members.find((e) => e.email === ($user?.email ?? '') && e.name === ($user?.displayName ?? ''))}
-											<Button
-												variant="destructive"
-												on:click={async () => {
-													const members = team.members;
-													members.splice(
-														members.findIndex(
-															(e) =>
-																e.email === ($user?.email ?? '') &&
-																e.name === ($user?.displayName ?? '')
-														),
-														1
-													);
-													await setDoc(
-														doc(db, 'events', event.event ?? ''),
-														{
-															teams: data.teams.filter(
-																// @ts-ignore
-																(t) => t.members.length > 0
-															)
-														},
-														{
-															merge: true
-														}
-													);
-												}}>Leave</Button
-											>
-										{:else}
-											<Button
-												class="bg-green-500 hover:bg-green-400"
-												on:click={async () => {
-													const members = team.members;
-													members.push({
-														name: $user?.displayName ?? '',
-														email: $user?.email ?? ''
-													});
-													await setDoc(
-														doc(db, 'events', event.event ?? ''),
-														{
-															teams: data.teams
-														},
-														{
-															merge: true
-														}
-													);
-												}}>Join</Button
-											>
-										{/if}
-									</Card.Title>
-									<Card.Content>
-										<ul>
-											{#each team.members as teamMember}
-												<li>{teamMember.name}</li>
-											{/each}
-										</ul>
-									</Card.Content>
-								</Card.Root>
-							{/each}
+							{#if event.event === 'Technology Bowl'}
+								<p>
+									The team for this event will be based off of a test which you will take at a later
+									date.
+								</p>
+							{:else}
+								{#each data.teams as te}
+									{@const team = correctType(te)}
+									<Card.Root class="bg-blue-500 bg-opacity-20">
+										<Card.Title class="m-2 ml-4">
+											{#if team.members.find((e) => e.email === ($user?.email ?? '') && e.name === ($user?.displayName ?? ''))}
+												<Button
+													variant="destructive"
+													on:click={async () => {
+														const members = team.members;
+														members.splice(
+															members.findIndex(
+																(e) =>
+																	e.email === ($user?.email ?? '') &&
+																	e.name === ($user?.displayName ?? '')
+															),
+															1
+														);
+														await setDoc(
+															doc(db, 'events', event.event ?? ''),
+															{
+																teams: data.teams.filter(
+																	// @ts-ignore
+																	(t) => t.members.length > 0
+																)
+															},
+															{
+																merge: true
+															}
+														);
+													}}>Leave</Button
+												>
+												<Dialog.Root>
+													<Tooltip.Root>
+														<Tooltip.Trigger>
+															<Dialog.Trigger
+																disabled={team.members.length >= (event.maxTeamSize ?? 9999)}
+															>
+																<Button
+																	class="bg-green-500 hover:bg-green-400"
+																	disabled={team.members.length >= (event.maxTeamSize ?? 9999)}
+																	>Add people</Button
+																>
+															</Dialog.Trigger>
+														</Tooltip.Trigger>
+														<Tooltip.Content>
+															<p>Team is full</p>
+														</Tooltip.Content>
+													</Tooltip.Root>
+
+													<Dialog.Content>
+														<Dialog.Title>Add People</Dialog.Title>
+														<Dialog.Description>
+															{#if team.members.length >= (event.maxTeamSize ?? 9999)}
+																<Alert variant="destructive" class="dark:brightness-200">
+																	<AlertTitle>Error</AlertTitle>
+																	<AlertDescription>This team is full</AlertDescription>
+																</Alert>
+															{:else}
+																<p>People who signed up for this event:</p>
+																<ul>
+																	{#each memberData.filter((m) => m.events.includes(event.event ?? '') && !data.teams.find( (// @ts-ignore
+																					t) => t.members.find((e) => e.email === m.email) )) as person}
+																		<li class="flex flex-row items-center">
+																			{person.name}
+																			<Button
+																				on:click={async () => {
+																					const members = team.members;
+																					members.push({
+																						name: person.name,
+																						email: person.email
+																					});
+																					await setDoc(
+																						doc(db, 'events', event.event ?? ''),
+																						{
+																							teams: data.teams
+																						},
+																						{
+																							merge: true
+																						}
+																					);
+																				}}
+																				variant="outline"
+																				size="icon"
+																				class="ml-2"><Plus /></Button
+																			>
+																		</li>
+																	{:else}
+																		<li>
+																			No one else singed up for this event. Please see a board
+																			member for next steps.
+																		</li>
+																	{/each}
+																</ul>
+															{/if}
+														</Dialog.Description>
+													</Dialog.Content>
+												</Dialog.Root>
+											{:else}
+												<p>
+													Ask someone in this team to add you if you want to join this team (you
+													must have signed up for this event)!
+												</p>
+											{/if}
+										</Card.Title>
+										<Card.Content>
+											<ul>
+												{#each team.members as teamMember}
+													<li>{teamMember.name}</li>
+												{/each}
+											</ul>
+										</Card.Content>
+									</Card.Root>
+								{/each}
+							{/if}
 						</Card.Content>
 						<Card.Footer>
 							<Button
+								disabled={// @ts-ignore
+								data.teams.find((t) =>
+									t.members.find(
+										// @ts-ignore
+										(e) => e.email === ($user?.email ?? '') && e.name === ($user?.displayName ?? '')
+									)
+								) || event.event === 'Technology Bowl'}
 								on:click={async () => {
 									data.teams.push({
 										members: [
