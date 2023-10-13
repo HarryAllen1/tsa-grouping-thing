50762b3c7f208c876113d7ce6faca5a08d7117a9
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2023-10-12 21:13:37 -0700

    more notes and stuff

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,4 +1,17 @@
 import type { DocumentData } from 'firebase/firestore';
 
 export const correctTeamsDataType = (data: DocumentData[string]) =>
-	data as { members: { name: string; email: string }[] }[];
+	data as {
+		members: { name: string; email: string }[];
+		teamCaptain?: string;
+		lastUpdatedBy?: string;
+	}[];
+
+export const correctDocType = (data: DocumentData) =>
+	data as {
+		teams: {
+			members: { name: string; email: string }[];
+			teamCaptain?: string;
+			lastUpdatedBy?: string;
+		}[];
+	};
