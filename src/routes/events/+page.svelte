6f4390b02bee0fb2c231357cf766a3983521f0a5
Harry Allen <66224939+HarryAllen1@users.noreply.github.com>
2023-10-26 12:08:05 -0700

    more security

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -75,6 +75,11 @@
 					id={event.event}
 					class="h-6 w-6 flex items-center justify-center [&>div]:w-6 [&>div]:h-6"
 					onCheckedChange={async () => {
+						if (
+							data.locked ||
+							(!eventMap[event.event] && ($userDoc?.events.length ?? 0) >= 6)
+						)
+							return;
 						await setDoc(doc(db, 'users', $user?.email ?? ''), {
 							events: eventMap[event.event]
 								? $userDoc?.events.filter((e) => e !== event.event) ?? []
