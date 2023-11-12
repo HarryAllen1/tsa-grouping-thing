ac514932e3ab905ea12a91d318ff36e2361b6fa7
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2023-11-11 16:06:06 -0800

    Update ColorKey.svelte with aspect-square class.

--- src/routes/stats/ColorKey.svelte
+++ src/routes/stats/ColorKey.svelte
@@ -2,25 +2,29 @@
 	class="swatch-wrapper flex flex-row [&>span]:flex [&>span]:flex-row [&>span]:gap-2 gap-4"
 >
 	<span>
-		<svg width="18" height="18" class="fill-blue-500">
+		<svg width="18" height="18" class="fill-blue-500 aspect-square">
 			<rect width="100%" height="100%" />
 		</svg>
 		Not enough people
 	</span>
 	<span>
-		<svg width="18" height="18" class="fill-black dark:fill-white">
+		<svg
+			width="18"
+			height="18"
+			class="fill-black dark:fill-white aspect-square"
+		>
 			<rect width="100%" height="100%" />
 		</svg>
 		Well below event limit
 	</span>
 	<span>
-		<svg width="18" height="18" class="fill-yellow-500">
+		<svg width="18" height="18" class="fill-yellow-500 aspect-square">
 			<rect width="100%" height="100%" />
 		</svg>
 		Nearing event limit
 	</span>
 	<span>
-		<svg width="18" height="18" class="fill-red-500">
+		<svg width="18" height="18" class="fill-red-500 aspect-square">
 			<rect width="100%" height="100%" />
 		</svg>
 		Exceeded event limit
