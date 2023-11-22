965bf94b6dcdc281c334d30372276fee7bd56076
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2023-11-22 00:12:10 -0800

    ANALYTICS!!!

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,6 @@
-import { partytownVite } from '@builder.io/partytown/utils';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { join } from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), partytownVite({
-		dest: join(process.cwd(), '.svelte-kit/output/client/~partytown')
-	})],
+	plugins: [sveltekit()],
 });
