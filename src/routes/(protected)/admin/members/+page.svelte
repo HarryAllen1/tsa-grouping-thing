47e65c0bb1302079b608b7940d752df70769e7e8
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2024-02-04 23:20:13 -0800

    add list view

--- src/routes/(protected)/admin/members/+page.svelte
+++ src/routes/(protected)/admin/members/+page.svelte
@@ -15,12 +15,17 @@
 	import { doc, setDoc } from 'firebase/firestore';
 	import Fuse from 'fuse.js';
 	import { ChevronsUpDown, Save } from 'lucide-svelte';
+	import TableView from './TableView.svelte';
 
 	let search = '';
 	let hidePeopleWithoutEvents = false;
 	let sortBy = {
 		value: 'firstName',
 		label: 'First Name',
+	};
+	let view = {
+		value: 'grid',
+		label: 'Grid',
 	};
 	$: fuse = new Fuse($allUsersCollection, {
 		keys: ['name', 'email', 'grade', 'events', 'nationalId', 'washingtonId'],
@@ -114,9 +119,6 @@
 		</span>
 	</p>
 
-	<p>Red = not enough events</p>
-	<p>Yellow = doesn't have a team for all events</p>
-
 	<Button
 		class="mb-4"
 		on:click={() => {
@@ -134,7 +136,8 @@
 		<Switch bind:checked={hidePeopleWithoutEvents} id="hide-people" />
 		<Label for="hide-people">Hide people without events</Label>
 	</div>
-	<div class="mb-2 flex items-center space-x-2">
+	<Label for="sortBy">Sort by</Label>
+	<div id="sortBy" class="mb-2 flex items-center space-x-2">
 		<Select.Root bind:selected={sortBy}>
 			<Select.Trigger class="w-[180px]">
 				<Select.Value placeholder="Sort by..." />
@@ -151,7 +154,21 @@
 			<Select.Input name="favoriteFruit" />
 		</Select.Root>
 	</div>
-
+	<Label for="view">View</Label>
+	<div id="view" class="mb-2 flex items-center space-x-2">
+		<Select.Root bind:selected={view}>
+			<Select.Trigger class="w-[180px]">
+				<Select.Value placeholder="View..." />
+			</Select.Trigger>
+			<Select.Content>
+				<Select.Group>
+					<Select.Item label="Grid" value="grid">Grid</Select.Item>
+					<Select.Item label="List" value="list">List</Select.Item>
+				</Select.Group>
+			</Select.Content>
+			<Select.Input name="favoriteFruit" />
+		</Select.Root>
+	</div>
 	<Input
 		class="mb-4"
 		bind:value={search}
@@ -160,206 +177,200 @@
 		placeholder="Search"
 	/>
 
-	<div
-		class="grid w-full grid-cols-1 items-center gap-4 sm:grid-cols-2 lg:items-start xl:grid-cols-3"
-	>
-		{#each $allUsersCollection.toSorted( (a, b) => (sortBy.value === 'firstName' ? a.name : a.lastName ?? '').localeCompare(sortBy.value === 'firstName' ? b.name : b.lastName ?? ''), ) as user (user.email)}
-			{@const hash = user.email.replaceAll('@', '').replaceAll('.', '')}
-			{#if valuesMap[user.email]}
-				<Card.Root class={results.includes(user) ? '' : 'hidden'}>
-					<Card.Header>
-						<Card.Title>
-							{user.name}
-							{#if user.grade}
-								({user.grade})
-							{/if}
-						</Card.Title>
-						<Card.Description>
-							<a href="mailto:{user.email}" class="underline">{user.email}</a>
-						</Card.Description>
-					</Card.Header>
-					<Card.Content class="flex flex-col gap-4">
-						<div class="flex items-center space-x-2">
-							<Switch
-								id="{hash}admin"
-								checked={user.admin}
-								onCheckedChange={async (e) => {
-									await setDoc(
-										doc(db, 'users', user.email),
-										{ admin: e, lastUpdatedBy: $userStore?.email ?? '' },
-										{ merge: true },
-									);
-								}}
-							/>
-							<Label for="{hash}admin">Admin</Label>
-						</div>
-						<div class="flex items-center space-x-2">
-							<Switch
-								id="{hash}random"
-								checked={user.random ?? false}
-								onCheckedChange={async (e) => {
-									await setDoc(
-										doc(db, 'users', user.email),
-										{
-											random: e,
-											lastUpdatedBy: $userStore?.email ?? '',
-										},
-										{ merge: true },
-									);
-								}}
-							/>
-							<Label for="{hash}random">Random switch</Label>
-						</div>
-						<Label>
-							<span class="mb-2"> T-shirt size </span>
-							<Select.Root
-								selected={user.tShirtSize
-									? { value: user.tShirtSize, label: user.tShirtSize }
-									: { value: 'null', label: 'Unspecified' }}
-								onSelectedChange={async (s) => {
-									if (s)
+	<p>Red = not enough events</p>
+	<p>Yellow = doesn't have a team for all events</p>
+	<p class="mb-4">
+		Event format: [event name] ([team number] [👑 if team captain])
+	</p>
+
+	{#if view.value === 'grid'}
+		<div
+			class="grid w-full grid-cols-1 items-center gap-4 sm:grid-cols-2 lg:items-start xl:grid-cols-3"
+		>
+			{#each $allUsersCollection.toSorted( (a, b) => (sortBy.value === 'firstName' ? a.name : a.lastName ?? '').localeCompare(sortBy.value === 'firstName' ? b.name : b.lastName ?? ''), ) as user (user.email)}
+				{@const hash = user.email.replaceAll('@', '').replaceAll('.', '')}
+				{#if valuesMap[user.email]}
+					<Card.Root class={results.includes(user) ? '' : 'hidden'}>
+						<Card.Header>
+							<Card.Title>
+								{user.name}
+								{#if user.grade}
+									({user.grade})
+								{/if}
+							</Card.Title>
+							<Card.Description>
+								<a href="mailto:{user.email}" class="underline">{user.email}</a>
+							</Card.Description>
+						</Card.Header>
+						<Card.Content class="flex flex-col gap-4">
+							<div class="flex items-center space-x-2">
+								<Switch
+									id="{hash}admin"
+									checked={user.admin}
+									onCheckedChange={async (e) => {
 										await setDoc(
 											doc(db, 'users', user.email),
-											{
-												tShirtSize: s.value,
-											},
+											{ admin: e, lastUpdatedBy: $userStore?.email ?? '' },
 											{ merge: true },
 										);
-								}}
-							>
-								<Select.Trigger class="mt-2 w-full">
-									<Select.Value placeholder="T-shirt size" />
-								</Select.Trigger>
-								<Select.Content>
-									<Select.Item value="WXS">WXS</Select.Item>
-									<Select.Item value="S">S</Select.Item>
-									<Select.Item value="M">M</Select.Item>
-									<Select.Item value="L">L</Select.Item>
-									<Select.Item value="XL">XL</Select.Item>
-									<Select.Item value="XXL">XXL</Select.Item>
-									<Select.Item value="XXXL">XXXL</Select.Item>
-									<Select.Item value="null">Unspecified</Select.Item>
-								</Select.Content>
-							</Select.Root>
-						</Label>
-						<div class="flex w-full max-w-sm flex-col gap-1.5">
-							<Label for="{hash}natid">National ID</Label>
-							<div class="flex flex-row gap-2">
-								<Input
-									bind:value={valuesMap[user.email].nationalId}
-									type="number"
-									id="{hash}natid"
-									placeholder="123456"
+									}}
 								/>
-								<Button
-									disabled={valuesMap[user.email].nationalId?.toString() ===
-										user.nationalId?.toString()}
-									size="icon"
-									on:click={async () => {
+								<Label for="{hash}admin">Admin</Label>
+							</div>
+							<div class="flex items-center space-x-2">
+								<Switch
+									id="{hash}random"
+									checked={user.random ?? false}
+									onCheckedChange={async (e) => {
 										await setDoc(
 											doc(db, 'users', user.email),
 											{
-												nationalId: parseInt(
-													(valuesMap[user.email].nationalId ?? 0).toString(),
-												),
+												random: e,
 												lastUpdatedBy: $userStore?.email ?? '',
 											},
 											{ merge: true },
 										);
 									}}
-								>
-									<Save />
-								</Button>
-							</div>
-						</div>
-						<div class="flex w-full max-w-sm flex-col gap-1.5">
-							<Label for="{hash}wtsaid">WTSA ID</Label>
-							<div class="flex flex-row gap-2">
-								<Input
-									bind:value={valuesMap[user.email].washingtonId}
-									type="number"
-									id="{hash}wtsaid"
-									placeholder="123456"
 								/>
-								<Button
-									disabled={valuesMap[user.email].washingtonId?.toString() ===
-										user.washingtonId?.toString()}
-									size="icon"
-									on:click={async () => {
-										await setDoc(
-											doc(db, 'users', user.email),
-											{
-												washingtonId: (
-													valuesMap[user.email].washingtonId ?? 0
-												).toString(),
-												lastUpdatedBy: $userStore?.email ?? '',
-											},
-											{ merge: true },
-										);
+								<Label for="{hash}random">Random switch</Label>
+							</div>
+							<Label>
+								<span class="mb-2"> T-shirt size </span>
+								<Select.Root
+									selected={user.tShirtSize
+										? { value: user.tShirtSize, label: user.tShirtSize }
+										: { value: 'null', label: 'Unspecified' }}
+									onSelectedChange={async (s) => {
+										if (s)
+											await setDoc(
+												doc(db, 'users', user.email),
+												{
+													tShirtSize: s.value,
+												},
+												{ merge: true },
+											);
 									}}
 								>
-									<Save />
-								</Button>
-							</div>
-						</div>
-						<Label>
-							<span class="mb-2"> Gender </span>
-							<Select.Root
-								selected={user.gender
-									? { value: user.gender, label: user.gender }
-									: { value: 'null', label: 'Unspecified' }}
-								onSelectedChange={async (s) => {
-									if (s)
-										await setDoc(
-											doc(db, 'users', user.email),
-											{
-												gender: s.value,
-											},
-											{ merge: true },
-										);
-								}}
-							>
-								<Select.Trigger class="mt-2 w-full">
-									<Select.Value placeholder="Gender" />
-								</Select.Trigger>
-								<Select.Content>
-									<Select.Item value="Male">Male</Select.Item>
-									<Select.Item value="Female">Female</Select.Item>
-									<Select.Item value="Non-Binary">Non-Binary</Select.Item>
-									<Select.Item value="null">Unspecified</Select.Item>
-								</Select.Content>
-							</Select.Root>
-						</Label>
-
-						{#if user.demographic}
-							<p>
-								Demographic: {user.demographic}
-							</p>
-						{/if}
-
-						<div class="w-full">
-							<Collapsible.Root>
-								<Collapsible.Trigger asChild let:builder>
+									<Select.Trigger class="mt-2 w-full">
+										<Select.Value placeholder="T-shirt size" />
+									</Select.Trigger>
+									<Select.Content>
+										<Select.Item value="WXS">WXS</Select.Item>
+										<Select.Item value="S">S</Select.Item>
+										<Select.Item value="M">M</Select.Item>
+										<Select.Item value="L">L</Select.Item>
+										<Select.Item value="XL">XL</Select.Item>
+										<Select.Item value="XXL">XXL</Select.Item>
+										<Select.Item value="XXXL">XXXL</Select.Item>
+										<Select.Item value="null">Unspecified</Select.Item>
+									</Select.Content>
+								</Select.Root>
+							</Label>
+							<div class="flex w-full max-w-sm flex-col gap-1.5">
+								<Label for="{hash}natid">National ID</Label>
+								<div class="flex flex-row gap-2">
+									<Input
+										bind:value={valuesMap[user.email].nationalId}
+										type="number"
+										id="{hash}natid"
+										placeholder="123456"
+									/>
 									<Button
-										builders={[builder]}
-										variant="ghost"
-										size="sm"
-										class="member-collapsible flex w-full items-center p-2 {user
-											.events.length < 4 || user.events.length > 6
-											? 'text-red-500'
-											: user.events
-														.map(
-															(e) =>
-																$eventsCollection
-																	.find((ev) => ev.event === e)
-																	?.teams.find((t) =>
-																		t.members.find(
-																			(m) => m.email === user.email,
-																		),
-																	) ?? null,
-														)
-														.filter((t) => t).length < 4
-												? 'text-orange-500'
+										disabled={valuesMap[user.email].nationalId?.toString() ===
+											user.nationalId?.toString()}
+										size="icon"
+										on:click={async () => {
+											await setDoc(
+												doc(db, 'users', user.email),
+												{
+													nationalId: parseInt(
+														(valuesMap[user.email].nationalId ?? 0).toString(),
+													),
+													lastUpdatedBy: $userStore?.email ?? '',
+												},
+												{ merge: true },
+											);
+										}}
+									>
+										<Save />
+									</Button>
+								</div>
+							</div>
+							<div class="flex w-full max-w-sm flex-col gap-1.5">
+								<Label for="{hash}wtsaid">WTSA ID</Label>
+								<div class="flex flex-row gap-2">
+									<Input
+										bind:value={valuesMap[user.email].washingtonId}
+										type="number"
+										id="{hash}wtsaid"
+										placeholder="123456"
+									/>
+									<Button
+										disabled={valuesMap[user.email].washingtonId?.toString() ===
+											user.washingtonId?.toString()}
+										size="icon"
+										on:click={async () => {
+											await setDoc(
+												doc(db, 'users', user.email),
+												{
+													washingtonId: (
+														valuesMap[user.email].washingtonId ?? 0
+													).toString(),
+													lastUpdatedBy: $userStore?.email ?? '',
+												},
+												{ merge: true },
+											);
+										}}
+									>
+										<Save />
+									</Button>
+								</div>
+							</div>
+							<Label>
+								<span class="mb-2"> Gender </span>
+								<Select.Root
+									selected={user.gender
+										? { value: user.gender, label: user.gender }
+										: { value: 'null', label: 'Unspecified' }}
+									onSelectedChange={async (s) => {
+										if (s)
+											await setDoc(
+												doc(db, 'users', user.email),
+												{
+													gender: s.value,
+												},
+												{ merge: true },
+											);
+									}}
+								>
+									<Select.Trigger class="mt-2 w-full">
+										<Select.Value placeholder="Gender" />
+									</Select.Trigger>
+									<Select.Content>
+										<Select.Item value="Male">Male</Select.Item>
+										<Select.Item value="Female">Female</Select.Item>
+										<Select.Item value="Non-Binary">Non-Binary</Select.Item>
+										<Select.Item value="null">Unspecified</Select.Item>
+									</Select.Content>
+								</Select.Root>
+							</Label>
+
+							{#if user.demographic}
+								<p>
+									Demographic: {user.demographic}
+								</p>
+							{/if}
+
+							<div class="w-full">
+								<Collapsible.Root>
+									<Collapsible.Trigger asChild let:builder>
+										<Button
+											builders={[builder]}
+											variant="ghost"
+											size="sm"
+											class="member-collapsible flex w-full items-center p-2 {user
+												.events.length < 4 || user.events.length > 6
+												? 'text-red-500'
 												: user.events
 															.map(
 																(e) =>
@@ -371,37 +382,53 @@
 																			),
 																		) ?? null,
 															)
-															.filter((t) => t).length < user.events.length
-													? 'text-yellow-500'
-													: ''}"
-									>
-										Events ({user.events.length})
-										<div class="flex-1" />
-										<ChevronsUpDown class="h-4 w-4" />
-									</Button>
-								</Collapsible.Trigger>
-								<Collapsible.Content>
-									<Button href="/events/{user.email}">Edit</Button>
-									{#each user.events as event}
-										{@const maybeTeam = $eventsCollection
-											.find((e) => e.event === event)
-											?.teams.find((t) =>
-												t.members.find((m) => m.email === user.email),
-											)}
-										<p class:text-red-500={!maybeTeam}>
-											{event}
+															.filter((t) => t).length < 4
+													? 'text-orange-500'
+													: user.events
+																.map(
+																	(e) =>
+																		$eventsCollection
+																			.find((ev) => ev.event === e)
+																			?.teams.find((t) =>
+																				t.members.find(
+																					(m) => m.email === user.email,
+																				),
+																			) ?? null,
+																)
+																.filter((t) => t).length < user.events.length
+														? 'text-yellow-500'
+														: ''}"
+										>
+											Events ({user.events.length})
+											<div class="flex-1" />
+											<ChevronsUpDown class="h-4 w-4" />
+										</Button>
+									</Collapsible.Trigger>
+									<Collapsible.Content>
+										<Button href="/events/{user.email}">Edit</Button>
+										{#each user.events as event}
+											{@const maybeTeam = $eventsCollection
+												.find((e) => e.event === event)
+												?.teams.find((t) =>
+													t.members.find((m) => m.email === user.email),
+												)}
+											<p class:text-red-500={!maybeTeam}>
+												{event}
 
-											{#if maybeTeam}
-												({maybeTeam.teamNumber}{#if maybeTeam.teamCaptain?.toLowerCase() === user.email?.toLowerCase()}👑{/if})
-											{/if}
-										</p>
-									{/each}
-								</Collapsible.Content>
-							</Collapsible.Root>
-						</div>
-					</Card.Content>
-				</Card.Root>
-			{/if}
-		{/each}
-	</div>
+												{#if maybeTeam}
+													({maybeTeam.teamNumber}{#if maybeTeam.teamCaptain?.toLowerCase() === user.email?.toLowerCase()}👑{/if})
+												{/if}
+											</p>
+										{/each}
+									</Collapsible.Content>
+								</Collapsible.Root>
+							</div>
+						</Card.Content>
+					</Card.Root>
+				{/if}
+			{/each}
+		</div>
+	{:else}
+		<TableView {results} />
+	{/if}
 </div>
---
+++ src/routes/(protected)/admin/members/TableView.svelte
@@ -0,0 +1,102 @@
+<script lang="ts">
+	import { db, user as userStore, type UserDoc, eventsCollection } from '$lib';
+	import * as Select from '$lib/components/ui/select';
+	import { Switch } from '$lib/components/ui/switch';
+	import * as Table from '$lib/components/ui/table';
+	import * as HoverCard from '$lib/components/ui/hover-card';
+	import { doc, setDoc } from 'firebase/firestore';
+	import UserCard from '../UserCard.svelte';
+
+	export let results: UserDoc[];
+</script>
+
+<Table.Root>
+	<Table.Header>
+		<Table.Row>
+			<Table.Head>Washington ID</Table.Head>
+			<Table.Head>Name</Table.Head>
+			<Table.Head>Switch</Table.Head>
+			<Table.Head>T-shirt</Table.Head>
+			<Table.Head>Events</Table.Head>
+		</Table.Row>
+	</Table.Header>
+	<Table.Body>
+		{#each results as user (user.email)}
+			<Table.Row>
+				<Table.Cell>{user.washingtonId}</Table.Cell>
+				<Table.Cell>
+					<HoverCard.Root>
+						<HoverCard.Trigger>
+							{user.name}
+						</HoverCard.Trigger>
+						<HoverCard.Content>
+							<UserCard {user} />
+						</HoverCard.Content>
+					</HoverCard.Root>
+				</Table.Cell>
+				<Table.Cell>
+					<Switch
+						checked={user.random ?? false}
+						onCheckedChange={async (e) => {
+							await setDoc(
+								doc(db, 'users', user.email),
+								{
+									random: e,
+									lastUpdatedBy: $userStore?.email ?? '',
+								},
+								{ merge: true },
+							);
+						}}
+					/>
+				</Table.Cell>
+				<Table.Cell>
+					<Select.Root
+						selected={user.tShirtSize
+							? { value: user.tShirtSize, label: user.tShirtSize }
+							: { value: 'null', label: 'Unspecified' }}
+						onSelectedChange={async (s) => {
+							if (s)
+								await setDoc(
+									doc(db, 'users', user.email),
+									{
+										tShirtSize: s.value,
+									},
+									{ merge: true },
+								);
+						}}
+					>
+						<Select.Trigger class="w-32">
+							<Select.Value placeholder="T-shirt size" />
+						</Select.Trigger>
+						<Select.Content>
+							<Select.Item value="WXS">WXS</Select.Item>
+							<Select.Item value="S">S</Select.Item>
+							<Select.Item value="M">M</Select.Item>
+							<Select.Item value="L">L</Select.Item>
+							<Select.Item value="XL">XL</Select.Item>
+							<Select.Item value="XXL">XXL</Select.Item>
+							<Select.Item value="XXXL">XXXL</Select.Item>
+							<Select.Item value="null">Unspecified</Select.Item>
+						</Select.Content>
+					</Select.Root>
+				</Table.Cell>
+				<Table.Cell class="text-right">
+					{#each user.events as event}
+						{@const maybeTeam = $eventsCollection
+							.find((e) => e.event === event)
+							?.teams.find((t) =>
+								t.members.find((m) => m.email === user.email),
+							)}
+						<p class:text-red-500={!maybeTeam}>
+							{event}
+
+							{#if maybeTeam}
+								({maybeTeam.teamNumber}{#if maybeTeam.teamCaptain?.toLowerCase() === user.email?.toLowerCase()}👑{/if})
+							{/if}
+						</p>
+					{/each}
+				</Table.Cell>
+			</Table.Row>
+		{/each}
+	</Table.Body>
+</Table.Root>
