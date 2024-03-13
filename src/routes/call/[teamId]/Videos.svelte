ab8e914490b7286cb7096c5a409ef4f686ea1dd6
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2024-03-12 22:59:34 -0700

    add hang up button

--- src/routes/call/[teamId]/Videos.svelte
+++ src/routes/call/[teamId]/Videos.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
 	import { db } from '$lib';
 	import { servers } from '$lib/calling';
+	import { Button } from '$lib/components/ui/button';
 	import {
 		DocumentReference,
 		doc,
@@ -13,7 +15,9 @@
 		onSnapshot,
 		CollectionReference,
 		updateDoc,
+		deleteDoc,
 	} from 'firebase/firestore';
+	import PhoneMissed from 'lucide-svelte/icons/phone-missed';
 	import { onDestroy, onMount } from 'svelte';
 
 	const pc = new RTCPeerConnection(servers);
@@ -21,6 +25,85 @@
 	let localVideoEl: HTMLVideoElement;
 	let remoteStream: MediaStream;
 	let localStream: MediaStream;
+
+	const createCall = async (
+		callDocRef: DocumentReference<DocumentData, DocumentData>,
+		offerCandidates: CollectionReference<DocumentData>,
+		answerCandidates: CollectionReference<DocumentData>,
+	) => {
+		console.log('create');
+
+		pc.addEventListener('icecandidate', (event) => {
+			if (event.candidate) addDoc(offerCandidates, event.candidate.toJSON());
+		});
+
+		const offerDescription = await pc.createOffer();
+		await pc.setLocalDescription(offerDescription);
+
+		setDoc(callDocRef, {
+			offer: {
+				type: offerDescription.type,
+				sdp: offerDescription.sdp,
+			},
+		});
+
+		onSnapshot(callDocRef, (ss) => {
+			const data = ss.data();
+
+			if (!pc.currentRemoteDescription && data?.answer) {
+				const answerDescription = new RTCSessionDescription(data.answer);
+				pc.setRemoteDescription(answerDescription);
+			}
+		});
+
+		onSnapshot(answerCandidates, (ss) => {
+			ss.docChanges().forEach((change) => {
+				if (change.type === 'added') {
+					const candidate = new RTCIceCandidate(change.doc.data());
+					pc.addIceCandidate(candidate);
+				}
+			});
+		});
+	};
+
+	const stopVideo = () => {
+		localStream?.getTracks().forEach((track) => track.stop());
+		remoteStream?.getTracks().forEach((track) => track.stop());
+	};
+
+	const joinCall = async (
+		callDocRef: DocumentReference<DocumentData, DocumentData>,
+		offerCandidates: CollectionReference<DocumentData>,
+		answerCandidates: CollectionReference<DocumentData>,
+	) => {
+		console.log('join');
+		pc.addEventListener('icecandidate', (event) => {
+			if (event.candidate) addDoc(answerCandidates, event.candidate.toJSON());
+		});
+
+		const callData = (await getDoc(callDocRef)).data();
+
+		const offerDescription = callData?.offer;
+		await pc.setRemoteDescription(new RTCSessionDescription(offerDescription));
+
+		const answerDescription = await pc.createAnswer();
+		await pc.setLocalDescription(answerDescription);
+
+		await updateDoc(callDocRef, {
+			answer: {
+				type: answerDescription.type,
+				sdp: answerDescription.sdp,
+			},
+		});
+
+		onSnapshot(offerCandidates, (ss) => {
+			ss.docChanges().forEach((change) => {
+				if (change.type === 'added') {
+					pc.addIceCandidate(new RTCIceCandidate(change.doc.data()));
+				}
+			});
+		});
+	};
 
 	onMount(async () => {
 		localStream = await navigator.mediaDevices.getUserMedia({
@@ -66,80 +149,7 @@
 		else createCall(callDocRef, offerCandidates, answerCandidates);
 	});
 
-	const createCall = async (
-		callDocRef: DocumentReference<DocumentData, DocumentData>,
-		offerCandidates: CollectionReference<DocumentData>,
-		answerCandidates: CollectionReference<DocumentData>,
-	) => {
-		pc.addEventListener('icecandidate', (event) => {
-			if (event.candidate) addDoc(offerCandidates, event.candidate.toJSON());
-		});
-
-		const offerDescription = await pc.createOffer();
-		await pc.setLocalDescription(offerDescription);
-
-		setDoc(callDocRef, {
-			offer: {
-				type: offerDescription.type,
-				sdp: offerDescription.sdp,
-			},
-		});
-
-		onSnapshot(callDocRef, (ss) => {
-			const data = ss.data();
-
-			if (!pc.currentRemoteDescription && data?.answer) {
-				const answerDescription = new RTCSessionDescription(data.answer);
-				pc.setRemoteDescription(answerDescription);
-			}
-		});
-
-		onSnapshot(answerCandidates, (ss) => {
-			ss.docChanges().forEach((change) => {
-				if (change.type === 'added') {
-					const candidate = new RTCIceCandidate(change.doc.data());
-					pc.addIceCandidate(candidate);
-				}
-			});
-		});
-	};
-	const joinCall = async (
-		callDocRef: DocumentReference<DocumentData, DocumentData>,
-		offerCandidates: CollectionReference<DocumentData>,
-		answerCandidates: CollectionReference<DocumentData>,
-	) => {
-		pc.addEventListener('icecandidate', (event) => {
-			if (event.candidate) addDoc(answerCandidates, event.candidate.toJSON());
-		});
-
-		const callData = (await getDoc(callDocRef)).data();
-
-		const offerDescription = callData?.offer;
-		await pc.setRemoteDescription(new RTCSessionDescription(offerDescription));
-
-		const answerDescription = await pc.createAnswer();
-		await pc.setLocalDescription(answerDescription);
-
-		await updateDoc(callDocRef, {
-			answer: {
-				type: answerDescription.type,
-				sdp: answerDescription.sdp,
-			},
-		});
-
-		onSnapshot(offerCandidates, (ss) => {
-			ss.docChanges().forEach((change) => {
-				if (change.type === 'added') {
-					pc.addIceCandidate(new RTCIceCandidate(change.doc.data()));
-				}
-			});
-		});
-	};
-
-	onDestroy(() => {
-		localStream?.getTracks().forEach((track) => track.stop());
-		remoteStream?.getTracks().forEach((track) => track.stop());
-	});
+	onDestroy(stopVideo);
 </script>
 
 <div class="container relative mt-8 aspect-video lg:mx-16">
@@ -152,4 +162,20 @@
 		<video class="h-full w-full" muted autoplay bind:this={localVideoEl} />
 	</div>
 	<div class="absolute"></div>
+</div>
+<div class="container flex justify-center">
+	<Button
+		on:click={async () => {
+			await deleteDoc(doc(db, 'calls', $page.params.teamId));
+
+			stopVideo();
+
+			goto('/');
+		}}
+		size="lg"
+		class="aspect-square p-2"
+		variant="destructive"
+	>
+		<PhoneMissed />
+	</Button>
 </div>
