9757a991e9b0601a555b06cbd805f5c60e4befa7
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2024-06-24 16:47:59 -0700

    collapse public stuff

--- src/routes/EventCard.svelte
+++ src/routes/EventCard.svelte
@@ -9,11 +9,15 @@
 	} from '$lib';
 	import { Button } from '$lib/components/ui/button';
 	import * as Card from '$lib/components/ui/card';
+	import * as Collapsible from '$lib/components/ui/collapsible';
 	import { doc, setDoc } from 'firebase/firestore';
+	import ChevronUp from 'lucide-svelte/icons/chevron-up';
 	import TeamCard from './TeamCard.svelte';
 
 	let { event, eventData }: { event: EventDoc; eventData: EventData[] } =
 		$props();
+
+	let collapsibleOpen = $state(false);
 </script>
 
 <Card.Root>
@@ -57,9 +61,29 @@
 				correct, contact a board member.
 			</p>
 		{/if}
-		{#each event.teams as team (team.id)}
-			<TeamCard {event} {team} />
-		{/each}
+		{#if event.teams.length <= 5}
+			{#each event.teams as team (team.id)}
+				<TeamCard {event} {team} />
+			{/each}
+		{:else}
+			<Collapsible.Root bind:open={collapsibleOpen} class="contents">
+				{#each event.teams.slice(0, 4) as team (team.id)}
+					<TeamCard {event} {team} />
+				{/each}
+				<Collapsible.Content class="contents">
+					{#each event.teams.slice(5) as team (team.id)}
+						<TeamCard {event} {team} />
+					{/each}
+				</Collapsible.Content>
+				<Collapsible.Trigger class="w-full">
+					<Button variant="ghost" size="sm" class=" w-full">
+						<ChevronUp
+							class="transition-transform {collapsibleOpen ? '' : 'rotate-180'}"
+						/>
+					</Button>
+				</Collapsible.Trigger>
+			</Collapsible.Root>
+		{/if}
 	</Card.Content>
 	{#if !event.locked && !(event.teamCreationLocked && event.teams.length >= event.perChapter) && event.teams.filter( (t) => t.requests
 					?.map((r) => r.email)
