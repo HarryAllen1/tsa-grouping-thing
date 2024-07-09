7d714f0cc7537b0a422925116080578719aaef82
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2024-07-08 21:23:41 -0700

    Don't init sentry on dev, format

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,21 +1,26 @@
-import {sequence} from '@sveltejs/kit/hooks';
+import { sequence } from '@sveltejs/kit/hooks';
 import * as Sentry from '@sentry/sveltekit';
 import type { Handle } from '@sveltejs/kit';
+import { dev } from '$app/environment';
 
-Sentry.init({
-    dsn: "https://0c7f0ce6a3dd0adedc74dfa71107da37@o4504343003791360.ingest.us.sentry.io/4507561453748224",
-    tracesSampleRate: 1
-})
+if (!dev)
+	Sentry.init({
+		dsn: 'https://0c7f0ce6a3dd0adedc74dfa71107da37@o4504343003791360.ingest.us.sentry.io/4507561453748224',
+		tracesSampleRate: 1,
+	});
 
-export const handle: Handle = sequence(Sentry.sentryHandle(), async ({ event, resolve }) => {
-	if (event.url.hostname === 'tsa-grouping-thing.vercel.app') {
-		return Response.redirect(
-			`https://teaming.jhstsa.org${event.url.pathname}${event.url.search}`,
-			301,
-		);
-	}
+export const handle: Handle = sequence(
+	Sentry.sentryHandle(),
+	async ({ event, resolve }) => {
+		if (event.url.hostname === 'tsa-grouping-thing.vercel.app') {
+			return Response.redirect(
+				`https://teaming.jhstsa.org${event.url.pathname}${event.url.search}`,
+				301,
+			);
+		}
 
-	const response = await resolve(event);
-	return response;
-});
+		const response = await resolve(event);
+		return response;
+	},
+);
 export const handleError = Sentry.handleErrorWithSentry();
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
-import { sentrySvelteKit } from "@sentry/sveltekit";
+import { sentrySvelteKit } from '@sentry/sveltekit';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sentrySvelteKit({
-        sourceMapsUploadOptions: {
-            org: "harry-allen",
-            project: "tsa-grouping-thing"
-        }
-    }), enhancedImages(), sveltekit()],
+	plugins: [
+		sentrySvelteKit({
+			sourceMapsUploadOptions: {
+				org: 'harry-allen',
+				project: 'tsa-grouping-thing',
+			},
+		}),
+		enhancedImages(),
+		sveltekit(),
+	],
 	build: {
 		target: 'es2022',
 	},
@@ -17,19 +21,5 @@ export default defineConfig({
 		esbuildOptions: {
 			target: 'es2022',
 		},
-		include: [
-			'fuse.js',
-			'cmdk-sv',
-			'tailwindcss/colors',
-			'@observablehq/plot',
-			'svelte-sonner',
-			'@vercel/analytics',
-			'sveltefire',
-			'firebase/app',
-			'firebase/auth',
-			'firebase/firestore',
-			'firebase/storage',
-			'd3',
-		],
 	},
 });
