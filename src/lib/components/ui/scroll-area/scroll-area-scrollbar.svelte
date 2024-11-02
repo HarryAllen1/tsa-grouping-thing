619c59b92666d2c830c531052c8acdfad8664b82
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2024-11-02 00:04:48 -0700

    animate tooltip

--- src/lib/components/ui/scroll-area/scroll-area-scrollbar.svelte
+++ src/lib/components/ui/scroll-area/scroll-area-scrollbar.svelte
@@ -20,9 +20,9 @@
 	class={cn(
 		'flex touch-none select-none transition-colors',
 		orientation === 'vertical' &&
-			'h-full w-2.5 border-l border-l-transparent p-[1px]',
+			'h-full w-2.5 border-l border-l-transparent p-px',
 		orientation === 'horizontal' &&
-			'h-2.5 flex-col border-t border-t-transparent p-[1px]',
+			'h-2.5 w-full border-t border-t-transparent p-px',
 		className,
 	)}
 	{...restProps}
