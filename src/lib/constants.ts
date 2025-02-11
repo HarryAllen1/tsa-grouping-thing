b55972881a5f2065fe47065ff5cf1af423065144
Harry Allen <66224939+HarryAllen1@users.noreply.github.com>
2025-02-11 09:37:25 -0800

    update event limits

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
-export const MAX_EVENTS = 5;
-export const MIN_EVENTS = 4;
+export const MAX_EVENTS = 3;
+export const MIN_EVENTS = 3;
 export const POINT_OF_CONTACT_EMAIL = 's-' + 'hallen' + '@' + 'lwsd.' + 'org';
