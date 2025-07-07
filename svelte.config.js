902b5eea967ec79b7eacc2756013c789fad45184
Harry Allen <66224939+HarryAllen1@users.noreply.github.com>
2025-07-07 00:20:22 -0700

    switch to cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapterVercel from '@sveltejs/adapter-vercel';
+import adapterCloudflare from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,9 +8,7 @@ const config = {
 	preprocess: [vitePreprocess()],
 
 	kit: {
-		adapter: adapterVercel({
-			runtime: 'nodejs22.x',
-		}),
+		adapter: adapterCloudflare(),
 	},
 };
 
