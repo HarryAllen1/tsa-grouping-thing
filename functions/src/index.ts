f03cfb8bc19fc70d5ac23653827404a886e7a5b4
Harry Allen <66224939+HarryAllen1@users.noreply.github.com>
2025-07-22 11:29:21 -0700

    actually export functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,13 @@
 export { onlyAllowLWSDEmails } from './auth';
 export { logEventChanges } from './db';
 export {
+	addTeamMember,
+	becomeTeamCaptain,
+	createTeam,
+	editCardboardBoatTeamName,
+	leaveTeam,
+} from './events';
+export {
 	sendRequest,
 	sendRequestApproval,
 	sendRequestDenial,
