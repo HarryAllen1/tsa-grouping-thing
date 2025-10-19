5bd120e9e523f2c6051f4840673bd1d29a0c6565
Harry Allen <66224939+HarryAllen1@users.noreply.github.com>
2025-10-19 02:22:46 +0100

    there is literally no server code so fix the sentry error by removing sentry

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,26 +1,14 @@
-import { sequence } from '@sveltejs/kit/hooks';
-import { handleErrorWithSentry, init, sentryHandle } from '@sentry/sveltekit';
 import type { Handle } from '@sveltejs/kit';
-import { dev } from '$app/environment';
+import { sequence } from '@sveltejs/kit/hooks';
 
-if (!dev)
-	init({
-		dsn: 'https://0c7f0ce6a3dd0adedc74dfa71107da37@o4504343003791360.ingest.us.sentry.io/4507561453748224',
-		tracesSampleRate: 1,
-	});
+export const handle: Handle = sequence(async ({ event, resolve }) => {
+	if (event.url.hostname === 'tsa-grouping-thing.vercel.app') {
+		return Response.redirect(
+			`https://teaming.jhstsa.org${event.url.pathname}${event.url.search}`,
+			301,
+		);
+	}
 
-export const handle: Handle = sequence(
-	sentryHandle(),
-	async ({ event, resolve }) => {
-		if (event.url.hostname === 'tsa-grouping-thing.vercel.app') {
-			return Response.redirect(
-				`https://teaming.jhstsa.org${event.url.pathname}${event.url.search}`,
-				301,
-			);
-		}
-
-		const response = await resolve(event);
-		return response;
-	},
-);
-export const handleError = handleErrorWithSentry();
+	const response = await resolve(event);
+	return response;
+});
